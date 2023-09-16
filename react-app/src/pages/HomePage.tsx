2a4bf6bd9a2b7486be2033759a19626d3a59f097
atari-monk <atari.monk1@gmail.com>
2023-09-16 16:36:49 +0200

    play sound

--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
 import './HomePage.css'
+import ReactHowler from 'react-howler'
+import audioFile from './../welcome.mp3'
+import { useState } from 'react'
 
 function HomePage() {
+  const [isPlaying, setIsPlaying] = useState(false)
+
   return (
     <div className="HomePage">
       <h2>Welcome to the Command App</h2>
@@ -15,6 +20,12 @@ function HomePage() {
         You can also make lists of commands for your specific projects and
         needs.
       </p>
+      <div>
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+        <ReactHowler src={audioFile} playing={isPlaying} />
+      </div>
     </div>
   )
 }
